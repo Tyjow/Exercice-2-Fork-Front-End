758e27a9a7a355ae7953909484e7a4121dcc32c8
tyjow <platon.jonathan@gmail.com>
2016-11-09 16:21:21 +0100

    update work 0.1.1

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function show(id) {
+	document.getElementById(id).style.visibility = "visible";
+	document.getElementById(id).style.opacity = 1;
+}
+function hide(id) {
+	document.getElementById(id).style.visibility = "hidden";
+	document.getElementById(id).style.opacity = 0;
+}
+
